f2a444ac01028ab03855ecd687a35aa1b0447f5f
a17alian <>
2021-07-20 23:33:51 +0200

    styled-components theme

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Title>My page</Title>
       </main>
-
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
