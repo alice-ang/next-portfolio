91f2469b1199ae1335f28c7cc9b22ebd61b39334
a17alian <>
2021-07-22 01:13:19 +0200

    block content

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -1,12 +1,16 @@
 import groq from "groq";
 import client from "../../client";
-import styled from "styled-components";
-
+import BlockContent from "@sanity/block-content-to-react";
 const Project = (props) => {
-  const { title = "Missing title", categories } = props;
+  const { title = "Missing title", categories, body } = props;
   return (
     <article>
       <h1>{title}</h1>
+      <BlockContent
+        blocks={body}
+        projectId={client.projectId}
+        dataset={client.dataset}
+      />
       {categories && (
         <ul>
           {categories.map((category) => (
@@ -20,7 +24,8 @@ const Project = (props) => {
 
 const query = groq`*[_type == "project" && slug.current == $slug][0]{
   title,
-  "categories": categories[]->title
+  "categories": categories[]->title,
+  body
 }`;
 
 Project.getInitialProps = async function (context) {
