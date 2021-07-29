f9a45689407f7c6dd9d22542e1200bada32f53e4
a17alian <>
2021-07-29 09:29:44 +0200

    Fixed styled components

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
-export default function Footer() {
-  const FooterContainer = styled.footer({
-    width: "100%",
-    marginTop: "1em",
-    bottom: 0,
-    textAlign: "center",
-  });
+const FooterContainer = styled.footer({
+  width: "100%",
+  marginTop: "1em",
+  bottom: 0,
+  textAlign: "center",
+});
 
-  const Heart = styled.span({
-    color: ({ theme }) => theme.colors.heart,
-  });
+const Heart = styled.span({
+  color: ({ theme }) => theme.colors.heart,
+});
+
+export default function Footer() {
   return (
     <>
       <FooterContainer>
