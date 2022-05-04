1647ffac0b55853c5c7db28b0cfc7d1006183c7b
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-05-04 17:47:15 +0200

    icon

--- pages/index.js
+++ pages/index.js
@@ -84,9 +84,23 @@ export default function Home({ props }) {
       <Head>
         <title>Alice Anglesjö | Portfolio</title>
         <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥑</text></svg>"
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
